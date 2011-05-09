4b12356a6a9680ef2a4ede03ef536cfd69421238
Saida Temirkhodjaeva <saida.temirkhodjaeva@gmail.com>
2011-05-09 18:11:00 +0500

    read fadfasdfadsfok

--- config/environment.rb
+++ config/environment.rb
@@ -22,13 +22,13 @@ Rails::Initializer.run do |config|
   config.gem 'haml'
   #config.gem "thoughtbot-paperclip", :lib => "paperclip"
   #config.gem "mislav-will_paginate", :lib => "will_paginate", :source => "http://gems.github.com"
-  # config.gem 'justinfrench-formtastic', :lib => 'formtastic', :source => 'http://gems.github.com'
+  config.gem 'justinfrench-formtastic', :lib => 'formtastic', :source => 'http://gems.github.com'
   
-  # config.gem 'tiny_mce'
+  config.gem 'tiny_mce'
   
   #config.gem 'seer'
   
-  # config.gem 'acts_as_commentable'
+  config.gem 'acts_as_commentable'
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
