340f621acb18b961528a76f85d1aec3dbdc182b5
Saida Temirkhodjaeva <saida.temirkhodjaeva@gmail.com>
2011-05-09 22:51:06 +0500

    hope it's the last commit

--- config/environment.rb
+++ config/environment.rb
@@ -24,11 +24,11 @@ Rails::Initializer.run do |config|
   #config.gem "mislav-will_paginate", :lib => "will_paginate", :source => "http://gems.github.com"
   config.gem 'justinfrench-formtastic', :lib => 'formtastic', :source => 'http://gems.github.com'
   
-  config.gem 'tiny_mce'
+  #config.gem 'tiny_mce'
   
   #config.gem 'seer'
   
-  config.gem 'acts_as_commentable'
+  #config.gem 'acts_as_commentable'
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
